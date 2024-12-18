7d43962d5b18b1e86ac19e24a0844bfbf06d7939
Konstantin Khrykin <ion.vkid@gmail.com>
2024-12-18 14:13:43 +0300

    Сброс поиска

--- src/components/Common/Search.tsx
+++ src/components/Common/Search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { FC } from "react";
+import { MagnifyingGlassIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { FC, useRef } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
@@ -22,6 +22,8 @@ const Search: FC<{ placeholder: string }> = ({ placeholder }) => {
     console.log(term);
   }, 300);
 
+  const inputRef = useRef<HTMLInputElement>(null);
+  const query = searchParams.get("query") || "";
   return (
     <div className={"relative flex flex-1 flex-shrink-0"}>
       <label htmlFor={"search"} className={"sr-only"}>
@@ -31,6 +33,7 @@ const Search: FC<{ placeholder: string }> = ({ placeholder }) => {
         className={
           "peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm putline-2 placeholder:text-gray-500"
         }
+        ref={inputRef}
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
         defaultValue={searchParams.get("query")?.toString()}
@@ -40,6 +43,18 @@ const Search: FC<{ placeholder: string }> = ({ placeholder }) => {
           "absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray peer-focus:text-gray-900"
         }
       />
+      {query.length > 0 && (
+        <button
+          className={"absolute right-2 top-2"}
+          onClick={(ev) => {
+            ev.preventDefault();
+            if (inputRef.current) inputRef.current.value = "";
+            handleSearch("");
+          }}
+        >
+          <XMarkIcon className={"w-6 text-foreground"} />
+        </button>
+      )}
     </div>
   );
 };
